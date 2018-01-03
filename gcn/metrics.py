cf0488170c0f27977fd8408fe6d3a64e9f008eaf
Zachery <bendd-backup01@qq.com>
2018-01-03 23:38:23 +0800

    Weighted softmax implement

--- gcn/metrics.py
+++ gcn/metrics.py
@@ -1,14 +1,43 @@
 import tensorflow as tf
+import numpy as np
 
+def weighted_softmax_cross_entropy(preds, labels, beta):
+    print('A')
+    # S = mask.reduce_sum(axis=0)
+    # Sm = S.max()
+    # S = (Sm-S)/beta*Sm + 1
+    # loss *= (labels*S).reduce_sum(axis=1)
+    
+    # build weight matrix
+    sum = tf.reduce_sum(labels, axis=0)   #sum by row
+    smax = tf.reduce_max(sum)
+    smax_tensor=tf.fill([1, 7], smax)
+    weighted = (smax_tensor-sum)/(beta*smax_tensor) + 1
+    weighted = tf.cast(weighted, dtype=tf.float32)
+
+    # '''version 1
+    y=tf.nn.softmax(preds)
+    y_=tf.cast(labels, dtype=tf.float32)
+    cross_entropy = y_ * tf.log(y)
+    # version 1 end'''
+
+    '''version 2
+    cross_entropy=tf.nn.softmax_cross_entropy_with_logits(logits=preds, labels=labels)
+    cross_entropy=tf.reshape(tf.reduce_sum(cross_entropy), [1, 7])
+    version 2 end'''
+    #calculate weighted-cross-entropy
+    w_cross_entropy = weighted * cross_entropy
+    loss=-1*tf.reduce_sum(w_cross_entropy)
+    return tf.reduce_mean(loss)
 
 def masked_softmax_cross_entropy(preds, labels, mask):
     """Softmax cross-entropy loss with masking."""
-    loss = tf.nn.softmax_cross_entropy_with_logits(logits=preds, labels=labels)
+    #origin
+    loss = tf.nn.softmax_cross_entropy_with_logits(logits=preds, labels=labels) 
     mask = tf.cast(mask, dtype=tf.float32)
     mask /= tf.reduce_mean(mask)
     loss *= mask
     return tf.reduce_mean(loss)
-
 
 def masked_accuracy(preds, labels, mask):
     """Accuracy with masking."""
