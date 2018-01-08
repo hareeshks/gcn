05c316b178846bb2552a2ee739f849dbdb3f2c85
Zachary <bendd-backup01@qq.com>
2018-01-08 19:46:06 +0800

    Add normalized triplet loss.
    By assign `config['normal']` by True to use normalized triplet loss. But
    it must be used under `config[loss_func]=triplet`.

--- gcn/models.py
+++ gcn/models.py
@@ -1,5 +1,6 @@
 from gcn.layers import DenseNet, GraphConvolution, Residual, FullyConnected, ConvolutionDenseNet
-from gcn.metrics import masked_accuracy, masked_softmax_cross_entropy, weighted_softmax_cross_entropy, triplet_softmax_cross_entropy
+from gcn.metrics import masked_accuracy, masked_softmax_cross_entropy, weighted_softmax_cross_entropy, \
+    triplet_softmax_cross_entropy, triplet_normalize_softmax_cross_entropy
 import tensorflow as tf
 import numpy as np
 from copy import copy
@@ -125,9 +126,12 @@ class GCN_MLP(object):
                 self.loss += weighted_softmax_cross_entropy(self.outputs, self.placeholders['labels'],
                                                             self.model_config['ws_beta'])
             elif self.model_config['loss_func'] == 'triplet':
-                    self.loss += triplet_softmax_cross_entropy(self.outputs, self.placeholders['labels'], self.placeholders['triplet'],
+                if self.model_config['normal']:
+                    self.loss += triplet_normalize_softmax_cross_entropy(self.outputs, self.placeholders['labels'], self.placeholders['triplet'],
                                                              self.placeholders['labels_mask'], self.model_config['MARGIN'], self.model_config['triplet_lamda'])
-                                                 
+                else:   #default triplet
+                    self.loss += triplet_softmax_cross_entropy(self.outputs, self.placeholders['labels'], self.placeholders['triplet'],
+                                                             self.placeholders['labels_mask'], self.model_config['MARGIN'], self.model_config['triplet_lamda'])                                     
             else:
                 self.loss += masked_softmax_cross_entropy(self.outputs, self.placeholders['labels'],
                                                       self.placeholders['labels_mask'])
